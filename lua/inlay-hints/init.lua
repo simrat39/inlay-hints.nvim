09d82b068dccaa2bdd928fe07b4fa48ee08f2228
Simrat Grewal <simrats169169@gmail.com>
2022-07-26 16:49:19 -0700

    Initial port from rust-tools.nvim
    * Only works on rust files for now

---
+++ lua/inlay-hints/init.lua
@@ -0,0 +1,39 @@
+local M = {
+	enable = nil,
+	disable = nil,
+	set = nil,
+	unset = nil,
+	cache = nil,
+	config = nil,
+}
+
+function M.setup(opts)
+	local config = require("inlay-hints.config")
+	M.config = config
+	config.setup(opts)
+
+	local inlay = require("inlay-hints.hints")
+	local hints = inlay.new()
+		M.enable = function()
+			inlay.enable(hints)
+		end
+		M.disable = function()
+			inlay.disable(hints)
+		end
+		M.set = function()
+			inlay.set(hints)
+		end
+		M.unset = function()
+			inlay.unset()
+		end
+		M.cache = function()
+			inlay.cache_render(hints)
+		end
+		M.render = function()
+			inlay.render(hints)
+		end
+
+	M.enable()
+end
+
+return M
