74c8a010a3f3d346537c0eeee276c84bb5622a9a
Simrat Grewal <simrats169169@gmail.com>
2022-07-28 16:38:01 -0700

    feat: Add initial virtual lines renderer

--- lua/inlay-hints/render/init.lua
+++ lua/inlay-hints/render/init.lua
@@ -1,9 +1,10 @@
-local eol = require("inlay-hints.render.eol")
+-- local eol = require("inlay-hints.render.eol")
+local virtline = require("inlay-hints.render.virtline")
 
 local M = {}
 
-function M.render(bufnr, ns, hints) 
-    eol.render(bufnr, ns, hints)
+function M.render(bufnr, ns, hints)
+	virtline.render(bufnr, ns, hints)
 end
 
 return M
