33a16e98a77aeb9a1d97f2453701325b768a05bc
Simrat Grewal <simrats169169@gmail.com>
2022-07-28 21:00:01 -0700

    cleanup(config): Remove unused options

--- lua/inlay-hints/config.lua
+++ lua/inlay-hints/config.lua
@@ -15,23 +15,10 @@ local defaults = {
     },
   },
 
-    -- Only show inlay hints for the current line
-    only_current_line = true,
+  -- Only show inlay hints for the current line
+  only_current_line = true,
 
   eol = {
-
-    -- whether to show parameter hints with the inlay hints or not
-    -- default: true
-    show_parameter_hints = true,
-
-    -- prefix for parameter hints
-    -- default: "<-"
-    parameter_hints_prefix = "<- ",
-
-    -- prefix for all the other hints (type, chaining)
-    -- default: "=>"
-    other_hints_prefix = "=> ",
-
     -- whether to align to the lenght of the longest line in the file
     max_len_align = false,
 
