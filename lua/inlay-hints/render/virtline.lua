2f68383ed3f2ae90c3f292ecb8907c4eccc979d4
Simrat Grewal <simrats169169@gmail.com>
2022-07-28 20:46:24 -0700

    feat(virtline): Support separate highlights for parameter/type hints

--- lua/inlay-hints/render/virtline.lua
+++ lua/inlay-hints/render/virtline.lua
@@ -1,4 +1,5 @@
 local ih = require("inlay-hints")
+local InlayHintKind = ih.kind
 
 local M = {}
 
@@ -9,33 +10,39 @@ end
 
 function M.render_line(line, line_hints, bufnr, ns)
   local opts = ih.config.options or {}
-    
-    local virt_text = ""
 
-    for _, hint in ipairs(line_hints) do
-      local spaces = hint.range.character - string.len(virt_text)
-      if spaces < 1 then
-        spaces = 1
-      end
+  local parameter_opts = opts.hints.parameter
+  local type_opts = opts.hints.type
 
-      virt_text = virt_text .. string.rep(" ", spaces) .. hint.label
+  local virt_lines = {}
+  local virt_text = ""
+
+  for _, hint in ipairs(line_hints) do
+    local spaces = hint.range.character - string.len(virt_text)
+    if spaces < 1 then
+      spaces = 1
     end
 
-    if virt_text ~= "" then
-      vim.api.nvim_buf_set_extmark(bufnr, ns, line, 0, {
-        virt_lines = {
-          { { virt_text, opts.highlight } },
-        },
-      })
-    end
+    table.insert(virt_lines, {
+      string.rep(" ", spaces) .. hint.label,
+      hint.kind == InlayHintKind.Type and type_opts.highlight
+        or parameter_opts.highlight,
+    })
+    virt_text = virt_text .. string.rep(" ", spaces) .. hint.label
+  end
+
+  vim.api.nvim_buf_set_extmark(bufnr, ns, line, 0, {
+    virt_lines = {
+      virt_lines,
+    },
+  })
 end
 
 function M.render(bufnr, ns, hints)
-
   clear_ns(ns, bufnr)
 
   for line, line_hints in pairs(hints) do
-      M.render_line(line, line_hints, bufnr, ns)
+    M.render_line(line, line_hints, bufnr, ns)
   end
 end
 
