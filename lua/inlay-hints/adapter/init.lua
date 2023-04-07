cd0e60170ca1e6a9421d15447c68e2b6772cbafb
moliva <oliva.miguelh@gmail.com>
2023-04-07 18:22:59 -0300

    feat: comment out tsserver adapter fttb

--- lua/inlay-hints/adapter/init.lua
+++ lua/inlay-hints/adapter/init.lua
@@ -5,9 +5,10 @@ local default = require("inlay-hints.adapter.default")
 local M = {}
 
 function M.adapt_request(client, bufnr, callback)
-  if client.name == "tsserver" then
-    tsserver.adapt_request(client, bufnr, callback)
-  elseif client.name == "clangd" then
+  -- if client.name == "tsserver" then
+  --   tsserver.adapt_request(client, bufnr, callback)
+  -- elseif client.name == "clangd" then
+  if client.name == "clangd" then
     clangd.adapt_request(client, bufnr, callback)
   else
     default.adapt_request(client, bufnr, callback)
